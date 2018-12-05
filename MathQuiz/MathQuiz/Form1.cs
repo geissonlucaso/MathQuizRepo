c9c1c0116df4f0753b77083e1becb2f1441c11db
Geisson Oliveira <geissonlucaso@gmail.com>
2018-12-05 14:35:25 -0200

    Add the CheckTheAnswer() method.

--- MathQuiz/MathQuiz/Form1.cs
+++ MathQuiz/MathQuiz/Form1.cs
@@ -33,7 +33,7 @@ namespace MathQuiz
         int timeLeft;
 
 
-        ///<summary>
+        /// <summary>
         /// //Start quiz by filling in all of the problems and start the timer.
         /// </summary>
         public void StartTheQuiz()
@@ -61,6 +61,19 @@ namespace MathQuiz
         }
 
 
+        /// <summary>
+        /// Check the answer to see if the user got everything right.
+        /// </summary>
+        /// <returns> True if the answer is correct, False otherwise </returns>
+        private bool CheckTheAnswer()
+        {
+            if (addend1 + addend2 == sum.Value)
+                return true;
+            else
+                return false;
+        }
+
+
         //Call the StartTheQuiz() method and unenable the Start Button 
         private void startButton_Click(object sender, EventArgs e)
         {
@@ -72,10 +85,22 @@ namespace MathQuiz
         //Timer event elapsed
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (timeLeft > 0)
+            if (CheckTheAnswer())
             {
-                //Display the new time left by updating the TimeLeft label.
-                timeLeft -= 1;
+                //If the CheckTheAnswer() returns true, then the use got the answer right. Stop the timer and show 
+                //the MessageBox.
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!", "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                // If CheckTheAnswer() return false, keep counting
+                // down. Decrease the time left by one second and 
+                // display the new time left by updating the 
+                // Time Left label.
+                // Display the new time left by updating the TimeLeft label.
+                timeLeft--;
                 timeLabel.Text = timeLeft + " seconds";
             }
             else
